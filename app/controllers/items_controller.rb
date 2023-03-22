7a2e32f9ce4ada1d403dfb46138cf1c4b3d3c3ca
ehachi <ehajianto@gmail.com>
2023-03-22 20:14:58 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+
+    if params[:user_id]
+
+    user = User.find(params[:user_id])
+    items = user.items
+    else
+      items = Item.all
+ 
   end
 
+  render json: items, include: :user
+
+end
+
+def show
+  items = Item.find(params[:id])
+  items.user
+
+  render json: items
+end
+
+def create
+  if params[:user_id]
+    user = User.find(params[:user_id])
+    item = user.items.create(create_params)
+  else
+    item = Item.create(create_params)
+    return render json: item, status: :created
+  end
+  render json: item, status: :created
+end
+
+
+
+private
+
+def create_params
+  params.permit(:name, :description, :price)
+end
+def render_not_found_response
+  render json: { error: "Dog house not found" }, status: :not_found
+end
 end
